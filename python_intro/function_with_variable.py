817e6f15ab86a8c3369e0498ada4d8b91db7fe49
Justin domanais <justindomanais15@gmail.com>
2025-12-05 21:25:11 +0800

    variable + function

---
+++ python_intro/function_with_variable.py
@@ -0,0 +1,12 @@
+first_name = "Justin"
+last_name = "Domanais"
+
+def display_full_name():
+    global last_name #global variable to change lastname
+    x = first_name + " " + "Bieber"
+    return x
+
+    
+print("Fullname: " + display_full_name()) 
+
+
---
+++ python_intro/variables.py
@@ -0,0 +1,24 @@
+# Many Values to Multiple Variables
+name, age, gender = "Justin", 22, "Male"
+
+print(name)
+print(age)
+print(gender)
+
+# One Value to Multiple Variables (Display item/product stock)
+
+product_1 = product_2 = product_3 = 10
+print(" ")
+print("Item 1: Stock", product_1)
+print("Item 2: Stock", product_2)
+print("Item 3: Stock", product_3)
+
+# print water level for irrigation arduino 
+water_symbol = "H20 = "
+water_level_percent = 80
+percent = "%"
+
+output_water_level = water_symbol + str(water_level_percent) + percent
+print(output_water_level)
+
+    
